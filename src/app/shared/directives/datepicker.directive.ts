c2b23d572a97da130c005a6de0732e9b729cab05
james chalmers <james.chalmers184@gmail.com>
2021-08-02 15:45:42 +0100

    first commit

---
+++ src/app/shared/directives/datepicker.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, Output, EventEmitter, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appDatepicker]'
+})
+export class DatepickerDirective implements OnInit {
+  // tslint:disable-next-line:no-output-native
+  @Output() public change = new EventEmitter();
+
+  constructor(private elementRef: ElementRef) { }
+
+  public ngOnInit() {
+    $(this.elementRef.nativeElement).datepicker({
+      dateFormat: 'mm/dd/yy',
+      changeYear: true,
+      yearRange: "-100:+0",
+      onSelect: (dateText) => {
+        this.change.emit(dateText);
+      }
+    });
+  }
+}
